fb86f67b5e758341807b6f774b820e3ddb3ecb9d
Ana Maria <mihalceanuana@gmail.com>
2020-06-21 20:49:37 +0300

    Solution for the chart per service approach

--- src/main/java/com/example/landmark/health/DatabaseConnectionHealthCheck.java
+++ src/main/java/com/example/landmark/health/DatabaseConnectionHealthCheck.java
@@ -1,16 +1,22 @@
 package com.example.landmark.health;
 
+import com.example.landmark.service.MuseumRepository;
+import com.example.landmark.service.MuseumService;
+import io.quarkus.launcher.shaded.com.google.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @Readiness
 @ApplicationScoped
 public class DatabaseConnectionHealthCheck implements HealthCheck {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConnectionHealthCheck.class);
 
     @ConfigProperty(name = "database.up", defaultValue = "true")
     private boolean databaseUp;
@@ -33,7 +39,7 @@ public class DatabaseConnectionHealthCheck implements HealthCheck {
 
     private void simulateDatabaseConnectionVerification() {
         if (!databaseUp) {
-            throw new IllegalStateException("Cannot contact database");
+            throw new IllegalStateException("Could not count the database objects");
         }
     }
 }
